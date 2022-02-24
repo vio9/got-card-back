8f96b7ac8a7d839cbd45a177b7d9f5ebfcd1f85e
violaine <e.violaine@yahoo.fr>
2022-02-24 15:56:41 +0100

    add cors in project to avoid cross origin pb

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 require("./models/dbConfig");
-
 const persosRoutes = require("./routes/persosController");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // add middleware
 app.use(bodyParser.json()); // recup data in json
+app.use(cors()); // data open
 app.use("/persos", persosRoutes);
 
 app.listen(5500, () => console.log("server started"));
